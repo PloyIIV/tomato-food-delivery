3482cfb6a3c612ac7f8f6eac0591ffe2c70a6222
PloyIIV <chadapan.y58@gmail.com>
2024-05-22 18:49:34 +0700

    commit

--- backend/server.js
+++ backend/server.js
@@ -7,28 +7,32 @@ import 'dotenv/config'
 import cartRouter from "./routes/cartRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
 
-//  app config
-const app = express();
-const PORT = 4000;
+async function init() {
+    //  app config
+    const app = express();
+    const PORT = 4000;
 
-// middleware
-app.use(express.json());
-app.use(cors());
+    // middleware
+    app.use(express.json());
+    app.use(cors());
 
-// db connection
-connectDB();
+    // db connection
+    connectDB();
 
-// api endpoints
-app.use("/api/food", foodRouter)
-app.use("/images", express.static('uploads'))
-app.use('/api/user', userRouter)
-app.use('/api/cart', cartRouter)
-app.use('/api/order', orderRouter)
+    // api endpoints
+    app.use("/api/food", foodRouter)
+    app.use("/images", express.static('uploads'))
+    app.use('/api/user', userRouter)
+    app.use('/api/cart', cartRouter)
+    app.use('/api/order', orderRouter)
 
-app.get('/', (req,res) => {
-    res.send("API working")
-});
+    app.get('/', (req,res) => {
+        res.send("API working")
+    });
 
-app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`)
-});
+    app.listen(PORT, () => {
+        console.log(`Server started on http://localhost:${PORT}`)
+    });
+}
+
+init();
