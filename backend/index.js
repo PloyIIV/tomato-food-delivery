7af06a826154881a8236940dc07d6f42a8fd7c2e
PloyIIV <chadapan.y58@gmail.com>
2024-05-24 20:18:16 +0700

    uncomment on backend

--- backend/index.js
+++ backend/index.js
@@ -26,8 +26,8 @@ async function init() {
     connectDB();
 
     // api endpoints
-    // app.use("/api/food", foodRouter)
-    // app.use("/images", express.static('uploads'))
+    app.use("/api/food", foodRouter)
+    app.use("/images", express.static('uploads'))
     app.use('/api/user', userRouter)
     app.use('/api/cart', cartRouter)
     app.use('/api/order', orderRouter)
--- backend/routes/foodRoutes.js
+++ backend/routes/foodRoutes.js
@@ -5,15 +5,15 @@ import multer from "multer";
 const foodRouter = express.Router();
 
 // Image Storage Engine
-// const storage = multer.diskStorage({
-//     destination: "uploads",
-//     filename: (req, file, cb) => {
-//         return cb(null, `${Date.now()}${file.originalname}`)
-//     }
-// })
+const storage = multer.diskStorage({
+    destination: "uploads",
+    filename: (req, file, cb) => {
+        return cb(null, `${Date.now()}${file.originalname}`)
+    }
+})
 
-// const upload = multer({ storage: storage })
-//upload.single("image") ,
+const upload = multer({ storage: storage })
+upload.single("image") ,
 foodRouter.post('/add',  addFood);
 foodRouter.get('/list', getFood);
 foodRouter.post('/remove', removeFood);
